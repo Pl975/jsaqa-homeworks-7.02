50d9ff61c37b50005cd18c784888186c923d07b4
pavel shuvalov <pmshuv975@gmail.com>
2023-08-29 23:37:17 +0300

    Solution of task 1

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,47 +1,37 @@
 module.exports = {
-  "env": {
-    // где будет исполняться наш код
+  env: {
     browser: true,
     es2023: true,
   },
 
-  // Здесь мы подключаем используемые плагины.
-  "plugins": ["react", "@typescript-eslint", "import", "prettier"],
+  plugins: ['react', '@typescript-eslint', 'import', 'prettier'],
 
-     
-  /* Правила для отслеживания линтером. Если пресеты покрывают не все ситуации, 
-    вы можете прописать нужные правила вручную. */
-  "rules": {
-    "no-debugger": "off",
-    "no-console": 0,
-    "prettier/prettier": 'error', // Обязательно!! Подсвечивает ошибки из Prettier.
-    "react/prop-types": 0 // Отключаем правило проверки передаваемых типов.
-
+  rules: {
+    'no-debugger': 'off',
+    'no-console': 0,
+    'prettier/prettier': 'error',
+    'react/prop-types': 0,
   },
 
   /* Пресет с настройками. prettier должен быть последним. 
     Он удаляет некоторые правила eslint из-за которых могут возникать конфликты. */
-  "extends": [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-    "plugin:react-hooks/recommended",
-    "plugin:jsx-a11y/recommended",
-    "plugin:import/recommended",
-    "airbnb",
-    "airbnb-typescript/base",
-    "prettier",
-    "plugin:prettier/recommended",
-    "prettier/@typescript-eslint"
-  ], 
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/recommended',
+    'airbnb',
+    'prettier',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+  ],
 
-  "parser": '@babel/eslint-parser', // Парсер для обработки jsx кода
+  parser: '@babel/eslint-parser',
 
-  "parserOptions": {
-    "ecmaVersion": "latest",
-    "sourceType": "module"
-  }
-
-}
-
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+};
